e532428d4ce91c88e27d9e482c1e2d84e9e3b1f9
Alex Shyba <alex.shyba@uniform.dev>
2025-05-05 09:14:51 -0700

    chore: whyattend component stub

---
+++ components/WhyAttend.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import type { ComponentInstance } from "@uniformdev/canvas";
+import type { UniformRichTextNodeProps } from "@uniformdev/canvas-vue";
+
+interface Props {
+  title: string;
+}
+
+defineProps<Props>();
+</script>
+<template>
+  <div>
+  
+  </div>
+</template>
--- components/canvasComponents.ts
+++ components/canvasComponents.ts
@@ -4,10 +4,11 @@ import {
 } from "@uniformdev/canvas-vue";
 import Page from "./Page.vue";
 import Hero from "./Hero.vue";
-
+import WhyAttend from "./WhyAttend.vue";
 // register your new components here
 const componentMap = {
   hero: Hero,
+  whyattend: WhyAttend,
   page: Page,
 };
 
